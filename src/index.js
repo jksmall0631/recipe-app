be7eed5a45b04ceb9d9e5dc84b2884224cd0051e
Joshua Small <jksmall0631@gmail.com>
2017-02-21 12:14:37 -0700

    set up basic file structure and router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom';
+import { createStore} from 'redux';
 import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router';
-import Routes from './routes';
+import { Router, Route, browserHistory } from 'react-router';
 import rootReducer from './reducers';
+import App from './components/App';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(rootReducer, devTools);
 
-render(
+const router = (
   <Provider store={store}>
-    <Routes history={browserHistory} />
-  </Provider>,
-  document.getElementById('root')
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+      </Route>
+    </Router>
+  </Provider>
 )
+
+ReactDOM.render(router, document.getElementById('root'))
